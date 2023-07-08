76899d3f26deea6526f376015cb9a8bbd1dc9fe6
Aditya Saravana <aditya.saravana@icloud.com>
2023-07-08 00:46:53 -0700

    Initial project

---
+++ Pitchpin/Pitchpin/PitchpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchpinApp.swift
+//  Pitchpin
+//
+//  Created by Aditya Saravana on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PitchpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
